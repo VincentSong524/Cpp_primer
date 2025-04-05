09234fc9ff5129a6b4e38a04288eba3417b6b81d
Wan Sen <wansen.song@icloud.com>
2025-04-05 09:47:00 +0800

    Initial Commit

---
+++ C++_primer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++_primer
+//
+//  Created by 宋万森 on 2025/4/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
