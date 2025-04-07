3b377ab35c0ca2f814b9362bdaba961bb2fb1462
Wan Sen <wansen.song@icloud.com>
2025-04-07 09:22:56 +0800

    练习1.5

--- practise/practise_1/1_4.cpp
+++ practise/practise_1/1_4.cpp
@@ -5,7 +5,12 @@ int main()
     std::cout << "Enter two numbers:" << std::endl;
     int n1 = 0, n2 = 0;
     std::cin >> n1 >> n2;
-    std::cout << n1 << " + " << n2 << " = " << n1+n2 << std::endl;
+    std::cout << n1 ;
+    std::cout << " + " ;
+    std::cout << n2 ;
+    std::cout << " = ";
+    std::cout << n1+n2; 
+    std::cout << std::endl;
     std::cout << n1 << " * " << n2 << " = " << n1*n2 << std::endl;
     return 0;
 }
