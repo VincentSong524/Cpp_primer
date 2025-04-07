bba727006565ecefd2ab31dcfa76f084eeb0f482
Wan Sen <wansen.song@icloud.com>
2025-04-07 10:06:36 +0800

    练习1.9

--- practise/practise_1/1_4_1.cpp
+++ practise/practise_1/1_4_1.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-    int sum = 0, val = 1;
+    int sum = 0, val = 50;
     // 只要 val 的值小于等于 10，while循环就会持续执行
-    while (val <= 10) {
+    while (val <= 100) {
         sum += val; // 将 sum + val 赋值给 sum
         ++val;      // 将 val 加 1
     }
-    std::cout << "Sum of 1 to 10 inclusive is "<< sum << std::endl;
+    std::cout << "Sum of 50 to 100 inclusive is "<< sum << std::endl;
 
     return 0;
 }
