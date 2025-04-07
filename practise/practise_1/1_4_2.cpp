de569acbd226e221ee8d308245032d5b8f1db69b
Wan Sen <wansen.song@icloud.com>
2025-04-07 10:21:53 +0800

    练习1.12

--- practise/practise_1/1_4_2.cpp
+++ practise/practise_1/1_4_2.cpp
@@ -6,5 +6,10 @@ int main()
     for (int val = 1; val <= 10; val++)
         sum += val;
     std::cout << "Sum of 1 to 10 inclusive is " << sum << std::endl;
+
+    sum = 0;
+    for (int i = -100; i <= 100; i++)
+        sum += i;
+    std::cout << "sum = " << sum << std::endl;
     return 0;
 }
