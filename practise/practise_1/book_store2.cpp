6434b8691456dcba97481f61dc73b6c15ad153d4
Wan Sen <wansen.song@icloud.com>
2025-04-08 09:18:08 +0800

    添加书店类头文件

--- practise/practise_1/book_store2.cpp
+++ practise/practise_1/book_store2.cpp
@@ -4,7 +4,7 @@
 int main()
 {
     Sales_item item1, item2;
-    std::cin >> item1 >> item2;                 // 读取一堆交易记录
+    std::cin >> item1 >> item2;                 // 读取一对交易记录
     std::cout << item1 + item2 << std::endl;    // 打印他们的和
     return 0;
 }
